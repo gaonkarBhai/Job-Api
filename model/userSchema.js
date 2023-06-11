9b1dcd2b6aa0b4aa365482078155a1839c63a91f
gaonkarBhai <naveengaonkar014@gmail.com>
2023-06-11 21:29:43 +0530

    registration-basic-setup

--- model/userSchema.js
+++ model/userSchema.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
   name:{
@@ -11,12 +13,27 @@ const userSchema = new mongoose.Schema({
     type:String,
     required:[true,'email must be provided'],
     match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'invalid email address'],
-    unique:[true,'replication of email']
+    unique:[true,'duplication of email ID']
   }  ,
   password:{
     type:String,
     required:[true,'password must be provided'],
   }  ,
 })
+
+
+// Before save password is hashed
+userSchema.pre('save',async function(){
+  this.password = await bcrypt.hash(this.password,10)
+})
+
+// after user register token is created
+userSchema.methods.createJWT = function async(){
+  return jwt.sign(
+    { userId: this._id, userName: this.name },
+    process.env.JWT_SECRET,
+    { expiresIn: process.env.JWT_LIFETIME }
+  );
+}
 
 module.exports = mongoose.model("User", userSchema);
